31e3927145f533a8d5a814645a053178289f128a
Ali Moghimi <mghmiali@gmail.com>
2025-01-13 13:49:36 +0000

    feat: main module

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from client.engine import Engine
+from config import logger
+from database.helper import init_db_instance
+from transformer import Agent
+
+
 def main():
-    pass
+    logger.info("Initializing Engine...")
+    engine = Engine()
+    logger.info("Engine initialized successfully.")
+    logger.info("Running Engine...")
+    engine.run()
+    logger.info("Engine run completed successfully.")
+
+    logger.info("Transforming data...")
+    transformer = Agent(engine.data)
+    tables = transformer.transform()
+
+    logger.info("Saving data to database...")
+    conn = init_db_instance()
+    for t, dataframe in tables.items():
+        if not dataframe.empty:
+            logger.info(f"\n{dataframe}")
+            conn.insert_table(dataframe, t)
+
+    logger.info("Process completed successfully")
 
 
 if __name__ == "__main__":
